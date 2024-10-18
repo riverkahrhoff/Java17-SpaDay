cf0086d3bc683d7a9500a16058de26762b9f6316
riverkahrhoff <river.kahrhoff@gmail.com>
2024-10-17 20:22:45 -0500

    chapter 15 studio complete

--- src/main/java/org/launchcode/models/User.java
+++ src/main/java/org/launchcode/models/User.java
@@ -1,16 +1,26 @@
 package org.launchcode.models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public class User {
+
+    @Size(min = 5, max = 15, message = "username must be between 5 and 15 characters.")
     private String username;
+
+    @Email(message = "Invalid email format")
     private String email;
+
+    @Size(min = 6, message = "Password must be at least 6 characters long")
     private String password;
 
-    public User() {
+    @NotNull(message = "passwords do not match")
+    private String verifyPassword;
 
-    }
+    public User() {}
 
     public User(String username, String email, String password) {
-        this();
         this.username = username;
         this.email = email;
         this.password = password;
@@ -38,6 +48,21 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+        checkPassword();
+    }
+
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
+        checkPassword();
+    }
+
+    private void checkPassword() {
+        if (password != null && verifyPassword != null && !password.equals(verifyPassword)) {
+            verifyPassword = null;
+        }
     }
 }
-
